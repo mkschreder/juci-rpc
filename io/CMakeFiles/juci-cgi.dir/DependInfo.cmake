beb4d96c5b2048ec92a1228605cb0899ce504c70
Martin Schröder <mkschreder.uk@gmail.com>
2015-06-20 04:04:01 +0200

    Added source

---
+++ io/CMakeFiles/juci-cgi.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/data/intranet/software/rpconfig/ubus-mod-juci/io/main.c" "/data/intranet/software/rpconfig/ubus-mod-juci/io/CMakeFiles/juci-cgi.dir/main.c.o"
+  "/data/intranet/software/rpconfig/ubus-mod-juci/io/multipart_parser.c" "/data/intranet/software/rpconfig/ubus-mod-juci/io/CMakeFiles/juci-cgi.dir/multipart_parser.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".."
+  "../ubus"
+  "../uci"
+  "../ubus-rpcd/include"
+  "../json-c-0.11"
+  "io/../.."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ io/CMakeFiles/juci-cgi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juci-cgi.dir/main.c.o"
+  "CMakeFiles/juci-cgi.dir/multipart_parser.c.o"
+  "juci-cgi.pdb"
+  "juci-cgi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/juci-cgi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
